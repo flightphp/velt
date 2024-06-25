8fa23494ce0fc67f6656cc9a609848a5c39c31f5
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-24 22:33:16 -0400

    Cached root() return value

--- public/index.php
+++ public/index.php
@@ -6,7 +6,13 @@ const ROOT = __DIR__ . '/..';
 require ROOT . '/vendor/autoload.php';
 
 function root(): string {
-  return str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
+  static $value = null;
+
+  if ($value === null) {
+    $value = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
+  }
+
+  return $value;
 }
 
 Flight::set('flight.views.path', ROOT . '/app/views');
