f2b44cca31fce93e8e47b909720e375feba0f6c8
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-24 22:33:03 -0400

    Simplified layout

--- app/views/layouts/base.php
+++ app/views/layouts/base.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width" />
     <title><?= $title ?? 'Svelte app' ?></title>
     <link rel="icon" type="image/png" href="<?= root() ?>/favicon.png" />
     <link rel="stylesheet" href="<?= root() ?>/global.css" />
